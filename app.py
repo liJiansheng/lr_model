7dc9e576855856f6a97c6b9d5f8683b5654149d2
LiJiansheng <leekinsung@gmail.com>
2020-03-03 13:49:59 +0800

    add flask files

--- app.py
+++ app.py
@@ -1,44 +1,35 @@
-from flask import Flask, request, json
-import boto3
+import pandas as pd
+from flask import Flask, jsonify, request,json
 import pickle
-import joblib
-from boto.s3.key import Key
-from boto.s3.connection import S3Connection
 
+model = pickle.load(open('model.pkl','rb'))
 
-BUCKET_NAME = 'newsstorage'
-MODEL_FILE_NAME = 'lr_model.pkl'
-MODEL_LOCAL_PATH = './' + MODEL_FILE_NAME
 app = Flask(__name__)
-S3 = boto3.client('s3', region_name='ap-southeast-1')
+
 @app.route('/', methods=['POST'])
-def index():    
-    # Parse request body for model input 
+def predict():
+    # get data
+    #data = request.get_json(force=True)
+
+    # convert data into dataframe
+    #data.update((x, [y]) for x, y in data.items())
+    #data_df = pd.DataFrame.from_dict(data)
+
     body_dict = json.loads(request.get_data().decode('utf-8')) 
-    data = body_dict['0']     
+    data = body_dict['0']
+    # predictions
     
-    # Load model
-    model = load_model(MODEL_FILE_NAME)
-# Make prediction 
-    prediction = model.predict(data).tolist()
-# Respond with prediction result
-    result = {'prediction': prediction}    
-   
-    return json.dumps(result)
+    prediction=[]
+    for v in data.values():
+        p=model.predict([v]).tolist()
+        #print(p)
+        prediction.append(p[0])
+#prediction = model.predict([data['0']]).tolist()
+        #print(prediction)
+    result = {'prediction': prediction}   
 
+    # return data
+    return jsonify(result)
 
-def load_model(key):
-     # Load model from S3 bucket
-    #key_obj = Key(BUCKET_NAME)
-    #key_obj.key = MODEL_FILE_NAME
-    response = S3.get_object(Bucket=BUCKET_NAME, Key=key)
-# Load pickle model
-    model_str = response['Body'].read()     
-    model = pickle.loads(model_str)
-   
-    return model
-
-'''def predict(data):
-      # Process your data, create a dataframe/vector and make your predictions
-    final_formatted_data = []
-    return load_model().predict(final_formatted_data)'''
+if __name__ == '__main__':
+    app.run(port = 5000, debug=True)
