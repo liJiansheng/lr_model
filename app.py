5ee2eb612cd4d31dad713aa9f1394dc83e1d04d5
LiJiansheng <leekinsung@gmail.com>
2020-03-03 20:19:57 +0800

    app

--- app.py
+++ app.py
@@ -9,16 +9,11 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def predict():
     # get data
-    #data = request.get_json(force=True)
-
-    # convert data into dataframe
-    #data.update((x, [y]) for x, y in data.items())
-    #data_df = pd.DataFrame.from_dict(data)
-
+   
     body_dict = json.loads(request.get_data().decode('utf-8')) 
     data = body_dict['0']
     # predictions
-    
+
     prediction=[]
     for v in data.values():
         p=model.predict([v]).tolist()
@@ -29,7 +24,7 @@ def predict():
     result = {'prediction': prediction}   
 
     # return data
-    return jsonify(result)
+    return jsonify(prediction)
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
